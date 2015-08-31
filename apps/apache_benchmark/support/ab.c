c941ba0c7b8fdf71ccdf2cbbf0bacb98519c9389
Vincent Li <vincent.mc.li@gmail.com>
2015-08-31 15:13:34 -0700

    Add SSL support for apachebench on top of mTCP and DPDK

--- apps/apache_benchmark/support/ab.c
+++ apps/apache_benchmark/support/ab.c
@@ -442,6 +442,221 @@ static void apr_err(char *s, apr_status_t rv)
  *
  */
 #ifdef USE_SSL
+
+#ifndef OPENSSL_NO_SOCK
+
+#include <openssl/bio.h>
+#define get_last_socket_error() errno
+#define clear_socket_error()    errno=0
+
+
+static int mtcp_sock_write(BIO *h, const char *buf, int num);
+static int mtcp_sock_read(BIO *h, char *buf, int size);
+static int mtcp_sock_puts(BIO *h, const char *str);
+static long mtcp_sock_ctrl(BIO *h, int cmd, long arg1, void *arg2);
+static int mtcp_sock_new(BIO *h);
+static int mtcp_sock_free(BIO *data);
+int BIO_mtcp_sock_should_retry(int s);
+
+static BIO_METHOD mtcp_methods_sockp = {
+    BIO_TYPE_SOCKET,
+    "socket",
+    mtcp_sock_write,
+    mtcp_sock_read,
+    mtcp_sock_puts,
+    NULL,                       /* sock_gets, */
+    mtcp_sock_ctrl,
+    mtcp_sock_new,
+    mtcp_sock_free,
+    NULL,
+};
+
+BIO_METHOD *BIO_mtcp_s_socket(void)
+{
+    return (&mtcp_methods_sockp);
+}
+
+BIO *BIO_mtcp_new_socket(mctx_t mctx, int fd, int close_flag)
+{
+    BIO *ret;
+
+    ret = BIO_new(BIO_mtcp_s_socket());
+    if (ret == NULL)
+        return (NULL);
+    BIO_set_fd(ret, fd, close_flag);
+    ret->ptr = mctx;
+    return (ret);
+}
+
+static int mtcp_sock_new(BIO *bi)
+{
+    bi->init = 0;
+    bi->num = 0;
+    bi->ptr = NULL;
+    bi->flags = 0;
+    return (1);
+}
+
+static int mtcp_sock_free(BIO *a)
+{
+    if (a == NULL)
+        return (0);
+    mctx_t mctx = (mctx_t)a->ptr;
+    mtcp_close(mctx, a->num);
+/*
+    if (a->shutdown) {
+        if (a->init) {
+            //SHUTDOWN2(a->num);
+        }
+        a->init = 0;
+        a->flags = 0;
+    }
+*/
+    return (1);
+}
+
+static int mtcp_sock_read(BIO *b, char *out, int outl)
+{
+    int ret = 0;
+    mctx_t mctx = (mctx_t)b->ptr;
+
+    if (out != NULL) {
+        clear_socket_error();
+        //ret = readsocket(b->num, out, outl);
+        ret = mtcp_read(mctx, b->num, out, outl);
+        BIO_clear_retry_flags(b);
+        if (ret <= 0) {
+            if (BIO_mtcp_sock_should_retry(ret))
+                BIO_set_retry_read(b);
+        }
+    }
+    return (ret);
+}
+
+static int mtcp_sock_write(BIO *b, const char *in, int inl)
+{
+    int ret;
+    mctx_t mctx = (mctx_t)b->ptr;
+
+    clear_socket_error();
+    //ret = writesocket(b->num, in, inl);
+    ret = mtcp_write(mctx, b->num, in, inl);
+    BIO_clear_retry_flags(b);
+    if (ret <= 0) {
+        if (BIO_mtcp_sock_should_retry(ret))
+            BIO_set_retry_write(b);
+    }
+    return (ret);
+}
+
+static long mtcp_sock_ctrl(BIO *b, int cmd, long num, void *ptr)
+{
+    long ret = 1;
+    int *ip;
+
+    switch (cmd) {
+    case BIO_C_SET_FD:
+        mtcp_sock_free(b);
+        b->num = *((int *)ptr);
+        b->shutdown = (int)num;
+        b->init = 1;
+        break;
+    case BIO_C_GET_FD:
+        if (b->init) {
+            ip = (int *)ptr;
+            if (ip != NULL)
+                *ip = b->num;
+            ret = b->num;
+        } else
+            ret = -1;
+        break;
+    case BIO_CTRL_GET_CLOSE:
+        ret = b->shutdown;
+        break;
+    case BIO_CTRL_SET_CLOSE:
+        b->shutdown = (int)num;
+        break;
+    case BIO_CTRL_DUP:
+    case BIO_CTRL_FLUSH:
+        ret = 1;
+        break;
+    default:
+        ret = 0;
+        break;
+    }
+    return (ret);
+}
+
+static int mtcp_sock_puts(BIO *bp, const char *str)
+{
+    int n, ret;
+
+    n = strlen(str);
+    ret = mtcp_sock_write(bp, str, n);
+    return (ret);
+}
+
+int BIO_mtcp_sock_should_retry(int i)
+{
+    int err;
+
+    if ((i == 0) || (i == -1)) {
+        err = get_last_socket_error();
+
+        return (BIO_mtcp_sock_non_fatal_error(err));
+    }
+    return (0);
+}
+
+int BIO_mtcp_sock_non_fatal_error(int err)
+{
+    switch (err) {
+
+# ifdef EWOULDBLOCK
+#  ifdef WSAEWOULDBLOCK
+#   if WSAEWOULDBLOCK != EWOULDBLOCK
+    case EWOULDBLOCK:
+#   endif
+#  else
+    case EWOULDBLOCK:
+#  endif
+# endif
+
+# if defined(ENOTCONN)
+    case ENOTCONN:
+# endif
+
+# ifdef EINTR
+    case EINTR:
+# endif
+
+# ifdef EAGAIN
+#  if EWOULDBLOCK != EAGAIN
+    case EAGAIN:
+#  endif
+# endif
+
+# ifdef EPROTO
+    case EPROTO:
+# endif
+
+# ifdef EINPROGRESS
+    case EINPROGRESS:
+# endif
+
+# ifdef EALREADY
+    case EALREADY:
+# endif
+        return (1);
+        /* break; */
+    default:
+        break;
+    }
+    return (0);
+}
+
+#endif                          /* #ifndef OPENSSL_NO_SOCK */
+
 static long ssl_print_cb(BIO *bio,int cmd,const char *argp,int argi,long argl,long ret)
 {
     BIO *out;
@@ -613,7 +828,7 @@ static void ssl_proceed_handshake(struct connection *c)
         int ret, ecode;
         apr_pollfd_t new_pollfd;
         
-        ret = SSL_do_handshake(c->ssl);
+        ret = SSL_connect(c->ssl);
         ecode = SSL_get_error(c->ssl, ret);
 
         switch (ecode) {
@@ -1309,7 +1524,7 @@ static void start_connect(struct connection * c)
         }
         ssl_rand_seed();
         apr_os_sock_get(&fd, c->aprsock);
-        bio = BIO_new_socket(fd, BIO_NOCLOSE);
+        bio = BIO_mtcp_new_socket(g_mctx[_cpu], fd, BIO_NOCLOSE);
         SSL_set_bio(c->ssl, bio, bio);
         SSL_set_connect_state(c->ssl);
 		if (verbosity >= 4) {

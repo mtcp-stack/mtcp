c362aa6851dcb6cb94e77ac19197e9e67e022cd3
Asim Jamshed <ajamshed@ndsl.kaist.edu>
2016-10-23 23:13:40 +0900

    - Hopefully the last fix to this script.

--- .buildtest.sh
+++ .buildtest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd dpdk-16.04/tools
+cd dpdk-16.04/
 make install T=x86_64-native-linuxapp-gcc
-cd ../..
+cd ..
 cd dpdk
 rm -rf *
 ln -s ../dpdk-16.04/x86_64-native-linuxapp-gcc/lib/ lib

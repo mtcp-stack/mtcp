afe359b2ebeece83b89857dd305d2f9c1021e25b
Asim Jamshed <ajamshed@ndsl.kaist.edu>
2017-01-22 17:38:07 +0900

    - Forgot to update the travis shell script file.

--- .buildtest.sh
+++ .buildtest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-cd dpdk-16.04/
+cd dpdk-16.11/
 make install T=x86_64-native-linuxapp-gcc
 cd ..
 cd dpdk
 rm -rf *
-ln -s ../dpdk-16.04/x86_64-native-linuxapp-gcc/lib/ lib
-ln -s ../dpdk-16.04/x86_64-native-linuxapp-gcc/include include
+ln -s ../dpdk-16.11/x86_64-native-linuxapp-gcc/lib/ lib
+ln -s ../dpdk-16.11/x86_64-native-linuxapp-gcc/include include
 cd ..
 ./configure --with-dpdk-lib=`echo $PWD`/dpdk
 make

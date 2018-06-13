138a6a5da1d0786083fc155c599761ac5c85cc36
jingliu <jingliu@node0.test-cluster.distsched-pg0.wisc.cloudlab.us>
2018-06-13 09:42:37 -0500

    some test for link with both libos and mtcp

---
+++ apps/libos_mtcp/mtcp_queue.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+#include "mtcp_queue.h"
+
+int test(){
+    zeus_sgelem testem;
+    printf("hello\n");
+    return 0;
+}
+
---
+++ apps/libos_mtcp/test_so.c
@@ -0,0 +1,12 @@
+
+#include "mtcp_queue.h"
+
+/**
+ *
+ * export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/users/jingliu/datacenter-OS/libos/libmtcp/mtcp/apps/libos_mtcp
+ **/
+
+int main(){
+    test();
+    return 0;
+}

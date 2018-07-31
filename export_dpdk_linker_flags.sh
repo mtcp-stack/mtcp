28c5e13c02bf555e7c70e9d8ed19b8f16f02ac23
Asim Jamshed <ajamshed@ndsl.kaist.edu>
2018-07-31 04:01:08 -0400

    - Updated export_dpdk_linker_flags.

--- export_dpdk_linker_flags.sh
+++ export_dpdk_linker_flags.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sed -i -e 's/O_TO_EXE_STR =/\$(shell if [ \! -d \${RTE_SDK}\/\${RTE_TARGET}\/lib ]\; then mkdir \${RTE_SDK}\/\${RTE_TARGET}\/lib\; fi)\nLINKER_FLAGS = \$(call linkerprefix,\$(LDLIBS))\n\$(shell echo \${LINKER_FLAGS} \> \${RTE_SDK}\/\${RTE_TARGET}\/lib\/ldflags\.txt)\nO_TO_EXE_STR =/g' $RTE_SDK/mk/rte.app.mk
+if grep "ldflags.txt" $RTE_SDK/mk/rte.app.mk > /dev/null
+then
+    :
+else
+    sed -i -e 's/O_TO_EXE_STR =/\$(shell if [ \! -d \${RTE_SDK}\/\${RTE_TARGET}\/lib ]\; then mkdir \${RTE_SDK}\/\${RTE_TARGET}\/lib\; fi)\nLINKER_FLAGS = \$(call linkerprefix,\$(LDLIBS))\n\$(shell echo \${LINKER_FLAGS} \> \${RTE_SDK}\/\${RTE_TARGET}\/lib\/ldflags\.txt)\nO_TO_EXE_STR =/g' $RTE_SDK/mk/rte.app.mk
+fi

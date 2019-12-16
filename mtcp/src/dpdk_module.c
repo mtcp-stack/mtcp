4f278f799536198e074ebc9fca4706d57d70266b
Qingmin Liu <qingmin.liu@broadcom.com>
2019-12-16 10:44:40 +0800

    mtcp: src: limit RSS key len to 40 Bytes.
    Stingray has only 320-bit RSS key. So limit to 40B at the max.

--- mtcp/src/dpdk_module.c
+++ mtcp/src/dpdk_module.c
@@ -712,6 +712,8 @@ dpdk_load_module(void)
 			fflush(stdout);
 			if (!strncmp(dev_info[portid].driver_name, "net_mlx", 7))
 				port_conf.rx_adv_conf.rss_conf.rss_key_len = 40;
+			if (!strncmp(dev_info[portid].driver_name, "net_bnxt", 8))
+				port_conf.rx_adv_conf.rss_conf.rss_key_len = 40;
 			
 			ret = rte_eth_dev_configure(portid, CONFIG.num_cores, CONFIG.num_cores, &port_conf);
 			if (ret < 0)
